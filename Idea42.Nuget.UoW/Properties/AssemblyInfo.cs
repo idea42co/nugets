5837723683b75626a5c78d9695c8a35275aa513e
Ryan Ward <ryan@idea42.co>
2016-01-27 14:06:19 -0600

    This is the real nuget project. ;-)

--- Idea42.Nuget.UoW/Properties/AssemblyInfo.cs
+++ Idea42.Nuget.UoW/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Idea42.Nuget.UoW")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Idea42.NuGet.UoW")]
+[assembly: AssemblyDescription("Project template made by EyeCatch (http://www.eyecatch.no/)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Idea42.Nuget.UoW")]
+[assembly: AssemblyProduct("Idea42.NuGet.UoW")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4a428b9f-8d0c-49b3-bd16-5cede01607fb")]
+[assembly: Guid("5b586874-8d3e-4137-9ba8-e982413416d5")]
 
 // Version information for an assembly consists of the following four values:
 //
